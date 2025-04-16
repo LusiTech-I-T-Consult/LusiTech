be5f4b6b303177a35b8c3fcabceda22c43c68516
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2025-04-16 10:17:57 +0000

    Added waitlist form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import WaitlistForm
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def landing_page(request):
+    form = WaitlistForm()
+    return render(request, 'index.html', {'form': form})
+
+def join_waitlist(request):
+    if request.method == 'POST':
+        form = WaitlistForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for joining the waitlist!')
+            return redirect('index')  # or your landing page name
+    else:
+        form = WaitlistForm()
+    return render(request, 'index.html', {'form': form})
