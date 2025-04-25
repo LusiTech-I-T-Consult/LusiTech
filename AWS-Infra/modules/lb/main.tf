ba06f21abafbd38b144ed7d5f36ff0399db06be1
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2025-04-25 11:18:54 +0000

    Hot fix

--- AWS-Infra/modules/lb/main.tf
+++ AWS-Infra/modules/lb/main.tf
@@ -84,7 +84,9 @@ resource "aws_lb_target_group" "app_tg" {
 
 # HTTP Listener (redirect to HTTPS if certificate available, otherwise forward to target group)
 # HTTP Listener : Redirect to HTTPS
-resource "aws_lb_listener" "http" {
+# HTTP Listener: Redirect to HTTPS (only if certificate is provided)
+resource "aws_lb_listener" "http_redirect" {
+  count             = var.certificate_arn != "" ? 1 : 0
   load_balancer_arn = aws_lb.app_lb.arn
   port              = 80
   protocol          = "HTTP"
@@ -98,13 +100,18 @@ resource "aws_lb_listener" "http" {
       status_code = "HTTP_301"
     }
   }
-  dynamic "forward" {
-    for_each = var.certificate_arn == "" ? [1] : []
-    content {
-      target_group {
-        target_group_arn = aws_lb_target_group.app_tg.arn
-      }
-    }
+}
+
+# HTTP Listener: Forward directly to target group (only if certificate is NOT provided)
+resource "aws_lb_listener" "http_forward" {
+  count             = var.certificate_arn == "" ? 1 : 0
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
 
